9df2990e13e56c6fadb18e9348618bc36fd2236d
norhanmagdi <58911005+norhanmagdi@users.noreply.github.com>
2020-05-19 17:06:47 +0200

    transitions added

---
+++ SICXEAssembler/transitions.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+using namespace std;
+class transitions
+{
+public:
+	string subtract(string s1, string s2,int bits);
+	string binToHexa(string bin);
+	string hexaToBin(string hexa);
+	int hexaToDec(string hexa);
+	string decToHexa(int decimal);
+	int binToDec(string hexa);
+	string decToBin(int decimal);
+	int twoscomp(int num, int bits);
+	transitions();
+	~transitions();
+};
+
